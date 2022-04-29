40bd072f0c779c7c514af8b3ec84879bed10ef6a
Maxi Lovaiza <maximiliano.lovaiza@kdsoft.io>
2022-04-29 12:54:43 -0300

    prueba 3
    asfasf

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebase/8.10.1/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
 
 const app  = firebase.initializeApp({
     apiKey: 'AIzaSyDhZJ-6_gSC0rY0ldNQYbjzxPgwl3qeses',
