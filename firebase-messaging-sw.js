0145a6b482e53b086ef35f2b986aad89d1e769e6
Maxi Lovaiza <maximiliano.lovaiza@kdsoft.io>
2022-04-29 14:35:29 -0300

    cambios
    auosfasf

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,34 +1,19 @@
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
+);
 
-const app  = firebase.initializeApp({
-    apiKey: 'AIzaSyDhZJ-6_gSC0rY0ldNQYbjzxPgwl3qeses',
-    appId: '1:1051942780331:web:e871c416babdea5450a6ee',
-    messagingSenderId: '1051942780331',
-    projectId: 'flutter-web-a1352',
-    authDomain: 'flutter-web-a1352.firebaseapp.com',
-    storageBucket: 'flutter-web-a1352.appspot.com',
-    measurementId: 'G-J1H4CGPMVL',
+firebase.initializeApp({
+  apiKey: "AIzaSyDhZJ-6_gSC0rY0ldNQYbjzxPgwl3qeses",
+  appId: "1:1051942780331:web:e871c416babdea5450a6ee",
+  messagingSenderId: "1051942780331",
+  projectId: "flutter-web-a1352",
+  authDomain: "flutter-web-a1352.firebaseapp.com",
+  storageBucket: "flutter-web-a1352.appspot.com",
+  measurementId: "G-J1H4CGPMVL",
 });
 
-const messaging = firebase.messaging(app);
-// messaging
-//    .requestPermission()
-//    .then(function () {
-     
-//      console.log("Notification permission granted.");
-
-//      // get the token in the form of promiser
-//      return messaging.getToken()
-//    })
-//    .then(function(token) {
-//      // print the token on the HTML page
-//      console.log(token)
-//    })
-//    .catch(function (err) {
-   
-//    console.log("Unable to get permission to notify.", err);
-//  });
+const messaging = firebase.messaging();
 
 // Optional:
 messaging.onBackgroundMessage((message) => {
