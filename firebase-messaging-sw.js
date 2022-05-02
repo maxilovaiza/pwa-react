6b5b29f3489b8b46a5e5e09e9512a8a1131b306c
Maxi Lovaiza <maximiliano.lovaiza@kdsoft.io>
2022-05-02 12:42:01 -0300

    s

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.7.0/firebase-messaging.js"
-);
+importScripts("/__/firebase/8.10.1/firebase-app.js");
+importScripts('/__/firebase/8.10.1/firebase-messaging.js');
 const firebaseconfg = {
   apiKey: "AIzaSyDhZJ-6_gSC0rY0ldNQYbjzxPgwl3qeses",
     appId: "1:1051942780331:web:e871c416babdea5450a6ee",
@@ -17,6 +15,15 @@ firebase.initializeApp(firebaseconfg)
 const messaging = firebase.messaging();
 
 // Optional:
-messaging.onBackgroundMessage(function(payload){
-console.log(payload)
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
 });
