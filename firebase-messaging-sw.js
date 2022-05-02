57e469e7e05e784fcc76f7c635999b84bf46c148
Maxi Lovaiza <maximiliano.lovaiza@kdsoft.io>
2022-05-02 16:20:57 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.7.0/firebase-messaging.js');
 const firebaseconfg = {
   apiKey: "AIzaSyDhZJ-6_gSC0rY0ldNQYbjzxPgwl3qeses",
     appId: "1:1051942780331:web:e871c416babdea5450a6ee",
@@ -9,12 +9,12 @@ const firebaseconfg = {
     storageBucket: "flutter-web-a1352.appspot.com",
     measurementId: "G-J1H4CGPMVL",
 }
-firebase.initializeApp(firebaseconfg)
+initializeApp(firebaseconfg)
 
 
-const messaging = firebase.messaging();
+const messaging = messaging();
 
 // Optional:
-messaging.onBackgroundMessage(function(payload){
+onBackgroundMessage(function(payload){
 console.log(payload)
 });
