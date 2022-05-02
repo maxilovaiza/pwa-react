397bcac4ec55d6add1a4f7c9db9fb52cd6c51d81
Maxi Lovaiza <maximiliano.lovaiza@kdsoft.io>
2022-05-02 15:18:45 -0300

    s

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,22 @@
-// importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-// importScripts(
-//   "https://www.gstatic.com/firebasejs/9.7.0/firebase-messaging.js"
-// );
-// const firebaseconfg = {
-//   apiKey: "AIzaSyDhZJ-6_gSC0rY0ldNQYbjzxPgwl3qeses",
-//     appId: "1:1051942780331:web:e871c416babdea5450a6ee",
-//     messagingSenderId: "1051942780331",
-//     projectId: "flutter-web-a1352",
-//     authDomain: "flutter-web-a1352.firebaseapp.com",
-//     storageBucket: "flutter-web-a1352.appspot.com",
-//     measurementId: "G-J1H4CGPMVL",
-// }
-// firebase.initializeApp(firebaseconfg)
+importScripts("https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.7.0/firebase-messaging.js"
+);
+const firebaseconfg = {
+  apiKey: "AIzaSyDhZJ-6_gSC0rY0ldNQYbjzxPgwl3qeses",
+    appId: "1:1051942780331:web:e871c416babdea5450a6ee",
+    messagingSenderId: "1051942780331",
+    projectId: "flutter-web-a1352",
+    authDomain: "flutter-web-a1352.firebaseapp.com",
+    storageBucket: "flutter-web-a1352.appspot.com",
+    measurementId: "G-J1H4CGPMVL",
+}
+firebase.initializeApp(firebaseconfg)
 
 
-// const messaging = firebase.messaging();
+const messaging = firebase.messaging();
 
-// // Optional:
-// messaging.onBackgroundMessage(function(payload){
-// console.log(payload)
-// });
+// Optional:
+messaging.onBackgroundMessage(function(payload){
+console.log(payload)
+});
