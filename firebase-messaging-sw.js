77af49a54a8ba140ed38d703d41a237ca0c2c19b
Maxi Lovaiza <maximiliano.lovaiza@kdsoft.io>
2022-05-03 11:25:12 -0300

    s

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.7.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 const firebaseconfg = {
   apiKey: "AIzaSyDhZJ-6_gSC0rY0ldNQYbjzxPgwl3qeses",
     appId: "1:1051942780331:web:e871c416babdea5450a6ee",
@@ -9,12 +9,12 @@ const firebaseconfg = {
     storageBucket: "flutter-web-a1352.appspot.com",
     measurementId: "G-J1H4CGPMVL",
 }
-initializeApp(firebaseconfg)
+firebase.initializeApp(firebaseconfg)
 
 
-const messaging = messaging();
+const messaging = firebase.messaging();
 
 // Optional:
-onBackgroundMessage(function(payload){
+messaging.onBackgroundMessage(function(payload){
 console.log(payload)
 });
